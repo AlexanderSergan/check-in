667e8ff68c257b2bc667b2db831e651ef5692248
AlexanderSergan <veteroque@gmail.com>
2021-02-20 11:02:09 +0200

    ssr fix;

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
 import dayjs from 'dayjs'
 
 var localizedFormat = require('dayjs/plugin/localizedFormat')
